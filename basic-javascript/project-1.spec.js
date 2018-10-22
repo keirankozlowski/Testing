9ef4541dab6c404bbce1db8fdb70f170329d8012
Keiran <keirankozlowski@gmail.com>
2018-10-22 17:06:57 -0400

    Completed up to isOdd();

--- basic-javascript/project-1.spec.js
+++ basic-javascript/project-1.spec.js
@@ -1,5 +1,5 @@
 const helpers = require('./project-1');
-
+    
 // start testing!
 describe('project-1.js', () => {
 
@@ -28,10 +28,10 @@ describe('project-1.js', () => {
     })
     describe('areSameLength()', () => {
 
-        it('should return true if both strings are same length', () => {
+        it('should return true if strings are equal length', () => {
             const expected = true;
             
-            const actual = helpers.areSameLength('hello', 'teach');
+            const actual = helpers.areSameLength('meme', 'mama');
             
             expect(actual).toEqual(expected);
         });
@@ -48,4 +48,114 @@ describe('project-1.js', () => {
             expect(actual).toEqual(expected);
         });
     })
+
+    describe('lessThanNinety()', () => {
+
+        it('should return true if number less than 90', () => {
+            const expected = true;
+
+            const actual = helpers.lessThanNinety(85);
+
+            expect(actual).toEqual(expected);
+        });
+        it('should return false if number greater than 90', () => {
+            const expected = false;
+
+            const actual = helpers.lessThanNinety(95);
+
+            expect(actual).toEqual(expected);
+        });
+    })
+
+    describe('greaterThanFifty()', () => {
+
+        it('should return true if number greater than 50', () => {
+            const expected = true;
+
+            const actual = helpers.greaterThanFifty(55);
+
+            expect(actual).toEqual(expected);
+        });
+    })
+
+    describe('add()', () => {
+
+        it('should add together two values', () => {
+            const expected = 8;
+
+            const actual = helpers.add(4, 4);
+
+            expect(actual).toEqual(expected);
+        })
+    })
+
+    describe('subtract()', () => {
+
+        it('should subtract second value from the first', () => {
+            const expected = 6;
+
+            const actual = helpers.subtract(10, 4);
+
+            expect(actual).toEqual(expected);
+        })
+    })
+
+    describe('divide()', () => {
+
+        it('should divide first value by the second value', () => {
+            const expected = 3;
+
+            const actual = helpers.divide(15, 5);
+
+            expect(actual).toEqual(expected);
+        })
+    })
+
+    describe('multiply()', () => {
+
+        it('should find product of two values', () => {
+            const expected = 150;
+
+            const actual = helpers.multiply(75, 2);
+
+            expect(actual).toEqual(expected);
+        })
+    })
+
+    describe('getRemainder()', () => {
+
+        it('should find remainder of a quotient', () => {
+            const expected = 1;
+
+            const actual = helpers.getRemainder(10, 3);
+
+            expect(actual).toEqual(expected);
+        })
+    })
+
+    describe('isEven()', () => {
+
+        it('should return true if number is even', () => {
+            const expected = true;
+
+            const actual = helpers.isEven(2);
+
+            expect(actual).toEqual(expected);
+        })
+    })
+
+    describe('isOdd()', () => {
+
+        it('should return true if number is odd', () => {
+            const expected = true;
+            
+            const actual = helpers.isOdd(1);
+
+            expect(actual).toEqual(expected);
+        })
+    })
+
+
+    
+    
 });
